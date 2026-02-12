a5d48868f184195b6893b4db616ea1c916b5651f
TXMMYBXY <altusha_03@bk.ru>
2026-02-12 20:32:03 +0700

    изменения в бд

--- DocumentFlowAPI/Models/DocumentTemplatesModels/Template.cs
+++ DocumentFlowAPI/Models/DocumentTemplatesModels/Template.cs
@@ -6,8 +6,9 @@ public abstract class Template : EntityBase
 {
     public string Title { get; set; }
     public string Path { get; set; }
-    [ForeignKey(nameof(CreatedBy))]
     public int CreatedBy { get; set; }
+    [ForeignKey(nameof(CreatedBy))]
+    public virtual User User { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool IsActive { get; set; }
 }
